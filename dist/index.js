cbf267322471f9b851d66e62bed7e258da06f52a
quentinikeno <ikenoq@gmail.com>
2023-03-03 12:23:43 -1000

    Add buttons to display one of the two sections at the same time.

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,18 @@ const hexForm = document.getElementById("hex-form");
 const hexInput = document.getElementById("hex-input");
 const circle = document.getElementById("circle");
 const circleSpan = document.querySelector("#circle span");
+const binaryForm = document.getElementById("binary-form");
+const binaryInput = document.getElementById("binary-input");
+const decimalSpan = document.getElementById("decimal-span");
+const result = document.getElementById("result");
+const colorDisplayButton = document.getElementById("color-display-button");
+const binaryDisplayButton = document.getElementById("binary-display-button");
+const colorFinderSection = document.getElementById("color-finder");
+const binaryConverterSection = document.getElementById("binary-converter");
 hexForm.addEventListener("submit", handleHexSubmit);
+binaryForm.addEventListener("submit", handleBinarySubmit);
+colorDisplayButton.addEventListener("click", handleColorDisplayClick);
+binaryDisplayButton.addEventListener("click", handleBinaryDisplayClick);
 function handleHexSubmit(event) {
     event.preventDefault();
     const hex = hexInput.value;
@@ -15,4 +26,29 @@ function handleHexSubmit(event) {
     else {
         alert("Please enter a valid hex code and try again.");
     }
+}
+function handleBinarySubmit(event) {
+    event.preventDefault();
+    const decimal = binaryInput.value;
+    const binary = dec2binary(+decimal);
+    if (decimal) {
+        decimalSpan.textContent = decimal;
+        setTimeout(() => {
+            result.textContent = binary;
+        }, 500);
+    }
+    else {
+        alert("Please enter a number and try again.");
+    }
+}
+function dec2binary(dec) {
+    return (dec >>> 0).toString(2);
+}
+function handleColorDisplayClick() {
+    colorFinderSection.style.display = "block";
+    binaryConverterSection.style.display = "none";
+}
+function handleBinaryDisplayClick() {
+    binaryConverterSection.style.display = "block";
+    colorFinderSection.style.display = "none";
 }
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,22 @@ const hexInput = document.getElementById("hex-input")! as HTMLInputElement;
 const circle = document.getElementById("circle")!;
 const circleSpan = document.querySelector("#circle span")!;
 
+const binaryForm = document.getElementById("binary-form")!;
+const binaryInput = document.getElementById(
+	"binary-input"
+)! as HTMLInputElement;
+const decimalSpan = document.getElementById("decimal-span")!;
+const result = document.getElementById("result")!;
+
+const colorDisplayButton = document.getElementById("color-display-button")!;
+const binaryDisplayButton = document.getElementById("binary-display-button")!;
+const colorFinderSection = document.getElementById("color-finder")!;
+const binaryConverterSection = document.getElementById("binary-converter")!;
+
 hexForm.addEventListener("submit", handleHexSubmit);
+binaryForm.addEventListener("submit", handleBinarySubmit);
+colorDisplayButton.addEventListener("click", handleColorDisplayClick);
+binaryDisplayButton.addEventListener("click", handleBinaryDisplayClick);
 
 function handleHexSubmit(event: SubmitEvent): void {
 	event.preventDefault();
@@ -16,4 +31,33 @@ function handleHexSubmit(event: SubmitEvent): void {
 	} else {
 		alert("Please enter a valid hex code and try again.");
 	}
+}
+
+function handleBinarySubmit(event: SubmitEvent): void {
+	event.preventDefault();
+	const decimal = binaryInput.value;
+	const binary = dec2binary(+decimal);
+
+	if (decimal) {
+		decimalSpan.textContent = decimal;
+		setTimeout((): void => {
+			result.textContent = binary;
+		}, 500);
+	} else {
+		alert("Please enter a number and try again.");
+	}
+}
+
+function dec2binary(dec: number): string {
+	return (dec >>> 0).toString(2);
+}
+
+function handleColorDisplayClick(): void {
+	colorFinderSection.style.display = "block";
+	binaryConverterSection.style.display = "none";
+}
+
+function handleBinaryDisplayClick(): void {
+	binaryConverterSection.style.display = "block";
+	colorFinderSection.style.display = "none";
 }
