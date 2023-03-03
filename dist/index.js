a4e304ae72d08f1835863de2738ae8299606b58f
quentinikeno <ikenoq@gmail.com>
2023-03-03 10:55:53 -1000

    Create function to handle submission of the hex code form.

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,18 @@
 "use strict";
 const hexForm = document.getElementById("hex-form");
 const hexInput = document.getElementById("hex-input");
+const circle = document.getElementById("circle");
+const circleSpan = document.querySelector("#circle span");
 hexForm.addEventListener("submit", handleHexSubmit);
 function handleHexSubmit(event) {
     event.preventDefault();
-    console.log(hexInput.value);
+    const hex = hexInput.value;
+    const isHex = /^#[0-9A-F]{6}$/i.test(hex);
+    if (isHex) {
+        circle.style.backgroundColor = hex;
+        circleSpan.textContent = hex.toUpperCase();
+    }
+    else {
+        alert("Please enter a valid hex code and try again.");
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 const hexForm = document.getElementById("hex-form")!;
 const hexInput = document.getElementById("hex-input")! as HTMLInputElement;
+const circle = document.getElementById("circle")!;
+const circleSpan = document.querySelector("#circle span")!;
 
 hexForm.addEventListener("submit", handleHexSubmit);
 
-function handleHexSubmit(event: SubmitEvent) {
+function handleHexSubmit(event: SubmitEvent): void {
 	event.preventDefault();
+	const hex = hexInput.value;
+	const isHex = /^#[0-9A-F]{6}$/i.test(hex);
 
-	console.log(hexInput.value);
+	if (isHex) {
+		circle.style.backgroundColor = hex;
+		circleSpan.textContent = hex.toUpperCase();
+	} else {
+		alert("Please enter a valid hex code and try again.");
+	}
 }
